84c431d487e89d641169ac9c375896fbccc78dce
Pedro Argentati <pedro.argentatii@gmail.com>
2025-10-22 19:46:54 -0300

    Initial commit

---
+++ src/main/java/br/com/postech/techchallange_order/TechchallangeOrderApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallangeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallangeOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/techchallange_order/TechchallangeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallangeOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
