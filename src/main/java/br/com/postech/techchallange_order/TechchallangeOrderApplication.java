9c153d5925f21ad71dd2df4fbe5ede121bfdcefe
Pedro Argentati <pedro.argentatii@gmail.com>
2025-10-23 07:17:06 -0300

    Update POM and application class for Spring Cloud integration
    - Set project version to 0.0.1
    - Added Spring Cloud dependency management
    - Included Spring Cloud OpenFeign starter dependency
    - Enabled Feign clients in the main application class

--- src/main/java/br/com/postech/techchallange_order/TechchallangeOrderApplication.java
+++ src/main/java/br/com/postech/techchallange_order/TechchallangeOrderApplication.java
@@ -2,12 +2,14 @@ package br.com.postech.techchallange_order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TechchallangeOrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechchallangeOrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechchallangeOrderApplication.class, args);
+    }
 
 }
