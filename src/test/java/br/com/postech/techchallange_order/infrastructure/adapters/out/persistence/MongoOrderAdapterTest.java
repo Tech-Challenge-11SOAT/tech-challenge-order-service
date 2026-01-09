28491223dd0815ecd7749230758b2dd14e421419
Pedro Argentati <pedro.argentatii@gmail.com>
2026-01-09 11:33:52 -0300

    feat: clean up MercadoPagoAdapter and enhance unit tests for order persistence

--- src/test/java/br/com/postech/techchallange_order/infrastructure/adapters/out/persistence/MongoOrderAdapterTest.java
+++ src/test/java/br/com/postech/techchallange_order/infrastructure/adapters/out/persistence/MongoOrderAdapterTest.java
@@ -1,19 +1,22 @@
 package br.com.postech.techchallange_order.infrastructure.adapters.out.persistence;
 
-import br.com.postech.techchallange_order.domain.model.Order;
-import br.com.postech.techchallange_order.helpers.OrderMother;
-import br.com.postech.techchallange_order.infrastructure.adapters.out.mongodb.model.OrderDocument;
 import org.bson.types.ObjectId;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import br.com.postech.techchallange_order.domain.model.Order;
+import br.com.postech.techchallange_order.helpers.OrderMother;
+import br.com.postech.techchallange_order.infrastructure.adapters.out.mongodb.model.OrderDocument;
 
 @ExtendWith(MockitoExtension.class)
 class MongoOrderAdapterTest {
@@ -73,4 +76,3 @@ class MongoOrderAdapterTest {
         verify(repository, times(1)).save(any(OrderDocument.class));
     }
 }
-
